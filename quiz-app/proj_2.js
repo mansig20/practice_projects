d906364260a6a8bbd4abb44e7886b76918a28759
mansig20 <mansigehlot.2021cs@technonjr.org>
2024-02-19 23:51:39 +0530

    updated js file

--- quiz-app/proj_2.js
+++ quiz-app/proj_2.js
@@ -1,5 +1,67 @@
-const questions=[
+const questions = [
     {
-        question: ""
-    }
-]
+        question: "What does API stand for?",
+        answers: [
+            { text: "Advanced Programming Interface", correct: false },
+            { text: "Application Programming Interface", correct: true },
+            { text: "Automated Program Installation", correct: false },
+            { text: "Application Protocol Integration", correct: false }
+        ]
+    },
+    {
+        question: "What is the primary purpose of Git?",
+        answers: [
+            { text: " To create dynamic web applications", correct: false },
+            { text: "To provide web hosting services", correct: false },
+            { text: "To manage source code version control", correct: true },
+            { text: "To write HTML code", correct: false }
+        ]
+    },
+    {
+        question: "What does IDE stand for in software development?",
+        answers: [
+            { text: "Integrated Development Environment", correct: true },
+            { text: "Interactive Design Environment", correct: false },
+            { text: "Interface Design Enhancement", correct: false },
+            { text: "Internet Development Engine", correct: false }
+        ]
+    },
+    {
+        question: "Which protocol is used for secure communication over a computer network?",
+        answers: [
+            { text: "HTTP", correct: false },
+            { text: "TCP", correct: false },
+            { text: "FTP", correct: false },
+            { text: "HTTPS", correct: true }
+        ]
+    },
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button)
+    });
+}
+
+startQuiz();
